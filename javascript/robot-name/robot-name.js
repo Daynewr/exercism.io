48f927a4ea4c01d0488841099fecf793538efa1a
Dayne <daynewr@gmail.com>
2016-05-31 15:56:20 -0500

    Added test 4

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,7 +1,16 @@
-var RobotName = function(){};
+var nameHolder = [];
 
+var RobotName = function(){
+    this.name = generateName();
 
-RobotName.prototype.name = (function(){
+    while(nameHolder.indexOf(this.name) !== -1) {
+      this.name = generateName();
+    }
+
+    nameHolder.push(this.name);
+};
+
+var generateName = function(){
     var robotName = "";
     var possibleLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     var possibleNumber = '1234567890';
@@ -13,7 +22,12 @@ RobotName.prototype.name = (function(){
     for(var i=0; i < 3; i++){
         robotName += possibleNumber.charAt(Math.floor(Math.random() * possibleNumber.length));
     }
+
     return robotName;
-})();
+}
+
+//RobotName.prototype.name = new GenerateName();
+
+//RobotName.prototype.name = generateName();
 
 module.exports = RobotName;
