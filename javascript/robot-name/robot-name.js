5ae62baeaaf45fd7bd8dcad14b4de6a2d3bd394a
Dayne <daynewr@gmail.com>
2016-05-31 14:38:07 -0500

    Added first attempt.  Passes 2 cases.  Needs work.

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,21 +1,19 @@
-var RobotName = function(){
-    this.giveName = function(){
-        var robotName = "";
-        var possibleLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-        var possibleNumber = '1234567890';
+var RobotName = function(){};
 
-        for( var i=0; i < 2; i++ ){
-            robotName += possibleLetter.charAt(Math.floor(Math.random() * possibleLetter.length));
-        }
 
-        for(var i=0; i < 3; i++){
-            robotName += possibleNumber.charAt(Math.floor(Math.random() * possibleNumber.length));
-        }
-        return robotName;
+RobotName.prototype.name = (function(){
+    var robotName = "";
+    var possibleLetter = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var possibleNumber = '1234567890';
+
+    for( var i=0; i < 2; i++ ){
+        robotName += possibleLetter.charAt(Math.floor(Math.random() * possibleLetter.length));
     }
-};
 
-
-RobotName.prototype.name =
+    for(var i=0; i < 3; i++){
+        robotName += possibleNumber.charAt(Math.floor(Math.random() * possibleNumber.length));
+    }
+    return robotName;
+})();
 
 module.exports = RobotName;
